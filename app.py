41bdf382382b2ea122a6d5ef1eca8146f33400c4
glorasika <rasika.ara@student.mahidol.edu>
2024-01-17 21:17:21 +0700

    Able to request leave and see others' leave date

--- app.py
+++ app.py
@@ -16,7 +16,18 @@ mysql.init_app(app)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    cur = mysql.get_db().cursor()
+    queryStatement = (
+        f"SELECT e.first_name, e.last_name, l.leave_date FROM leaves AS l JOIN employees e on e.employee_id = l.employee_id;"
+    )
+    check_request = cur.execute(queryStatement)
+
+    if check_request > 0:
+        requested_leave = cur.fetchall()
+        return render_template("index.html", requested_leave=requested_leave)
+    
+    else:
+        return render_template("index.html", requested_leave=None)
 
 
 @app.route("/login/", methods=['GET', 'POST'])
@@ -27,8 +38,8 @@ def login():
     elif request.method == 'POST':
         loginForm = request.form
         username = loginForm['username']
-        queryStatement = f"SELECT * FROM employees WHERE username = '{username}'"
         cur = mysql.get_db().cursor()
+        queryStatement = f"SELECT * FROM employees WHERE username = '{username}'"
         numRow = cur.execute(queryStatement)
 
         if numRow > 0:
@@ -104,6 +115,40 @@ def register():
         return redirect("/")
     
     return render_template("register.html")
+
+
+@app.route('/request_leave', methods=['GET', 'POST'])
+def request_leave():
+    try:
+        username = session['username']
+    except:
+        flash('Please sign in first.', 'danger')
+        return redirect('/')
+
+    cur = mysql.get_db().cursor()
+    queryStatement = f"SELECT employee_id FROM employees WHERE username = '{username}'"
+    cur.execute(queryStatement)
+    user = cur.fetchone()
+
+    if request.method == 'POST':
+        requestForm = request.form
+        date = requestForm['datepicker']
+        reason = requestForm['body']
+        print(date)
+
+        cur1 = mysql.get_db().cursor()
+        queryStatement1 = (
+            f"INSERT INTO leaves(employee_id, leave_reason, leave_date) "
+            f"VALUES('{user[0]}', '{reason}', '{date}')"
+        )
+        print(queryStatement1)
+        cur1.execute(queryStatement1)
+        mysql.get_db().commit()
+        cur1.close()
+
+        flash('Successfully requested leave.', 'success')
+        return redirect('/')
+    return render_template('request_leave.html')
 
 
 @app.route('/logout')
